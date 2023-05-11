9b5db1746d57121dc7fd83a3e497d5c73e9bdf0c
Volkov Alexander <volkov.525@gmail.com>
2023-05-11 13:13:38 +0300

    start

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template lang="pug">
+header
+NuxtPage
+</template>
+
+<style scoped lang="scss">
+header {
+	height: 64px;
+	border-bottom: var(--border);
+	position: sticky;
+	top: 0;
+	z-index: 2;
+	background: var(--headerBg);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template lang="pug">
+Html
+	Head
+		Title CSS variables
+.page
+	// h2 Home
+	q-btn(label="test" color="primary")
+	p test
+	p(v-for="n in 50") Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex repudiandae blanditiis, numquam cum, reprehenderit veniam corporis ullam molestias nostrum reiciendis itaque laborum, error quia maiores iste sequi est voluptates enim.
+</template>
+
+<style scoped lang="scss">
+h2 {
+	font-size: 4rem;
+}
+.page {
+	padding: 2rem;
+}
+</style>
