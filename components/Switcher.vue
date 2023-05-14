a8ca4b56718b6205d690e8854fb4ddc9b092b8e6
Volkov Alexander <volkov.525@gmail.com>
2023-05-14 16:07:31 +0300

    useMotion works somehow

---
+++ components/Switcher.vue
@@ -0,0 +1,113 @@
+<template>
+	<svg
+		width="34"
+		height="34"
+		viewBox="0 0 34 34"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+		@click="theme"
+	>
+		<circle
+			id="hover"
+			cx="17"
+			cy="17"
+			r="17"
+		/>
+		<Motion
+			:initial="false"
+			:animate="{ rotate: rot }"
+			:transition="transition"
+			tag="path"
+			id="sun"
+			fill-rule="evenodd"
+			clip-rule="evenodd"
+			d="M17.0208 5L20.5416 8.52081H26.0208V14L29.0416 17.0208L26.0208 20.0416V25.5208H20.5416L17.0208 29.0416L13.5 25.5208H9.02081V21.0416L5 17.0208L9.02081 13V8.52081H13.5L17.0208 5ZM17 24C20.866 24 24 20.866 24 17C24 13.134 20.866 10 17 10C13.134 10 10 13.134 10 17C10 20.866 13.134 24 17 24Z"
+			fill="currentColor"
+		/>
+		<mask
+			id="mask0_787_74"
+			style="mask-type: alpha"
+			maskUnits="userSpaceOnUse"
+			x="9"
+			y="9"
+			width="16"
+			height="16"
+		>
+			<circle
+				cx="17"
+				cy="17"
+				r="8"
+				fill="currentColor"
+			/>
+		</mask>
+		<g mask="url(#mask0_787_74)">
+			<Motion
+				:initial="false"
+				:animate="{ scale: scale, x: transx }"
+				:transition="{ duration: 0.5, easing: 'ease-in' }"
+				tag="circle"
+				id="move"
+				cx="17"
+				cy="17"
+				r="5"
+				fill="currentColor"
+			/>
+		</g>
+	</svg>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { Motion } from 'motion/vue'
+import { spring } from 'motion'
+import { useQuasar } from 'quasar'
+const $q = useQuasar()
+
+const theme = () => {
+	rot.value += 180
+	if ($q.dark.isActive) {
+		scale.value = 1.6
+		transx.value = -6
+	} else {
+		scale.value = 1
+		transx.value = 0
+	}
+	$q.dark.toggle()
+}
+
+const rot = ref(0)
+const scale = ref(1.6)
+const transx = ref(-6)
+
+const transition = {
+	easing: spring({
+		stiffness: 600,
+		damping: 12,
+	}),
+}
+</script>
+
+<style scoped lang="scss">
+svg {
+	cursor: pointer;
+	// transform-origin: unset unset;
+	&:hover {
+		#hover {
+			fill: hsla(0, 0%, 0%, 0.1);
+		}
+	}
+}
+
+#hover {
+	fill: transparent;
+}
+#sun,
+#move {
+	transform-origin: 50% 50%;
+	transform-box: fill-box;
+}
+
+body.body--dark svg:hover #hover {
+	fill: hsla(0, 0%, 100%, 0.15);
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,25 @@
 <script setup lang="ts"></script>
 
 <template lang="pug">
-header
-NuxtPage
+div
+	header
+		q-btn(label="Next" color="primary" to="/test")
+		q-btn(label="back" color="primary" to="/")
+	NuxtPage
 </template>
 
 <style scoped lang="scss">
 header {
 	height: 64px;
+	line-height: 64px;
 	border-bottom: var(--border);
 	position: sticky;
 	top: 0;
 	z-index: 2;
 	background: var(--headerBg);
+	padding: 0 2rem;
+	display: flex;
+	align-items: center;
+	gap: 1rem;
 }
 </style>
